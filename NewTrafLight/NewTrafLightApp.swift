a78b9ca163f175598fed41a82c529eb4d3984bb6
aldvs <andrewgogol777@gmail.com>
2022-11-21 22:04:57 +0300

    Initial Commit

---
+++ NewTrafLight/NewTrafLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewTrafLightApp.swift
+//  NewTrafLight
+//
+//  Created by admin on 21.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewTrafLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
